e247985a07ba3e0d6046f819759116f9493292df
YuliaBondar <uliabondar359@gmail.com>
2024-10-15 18:54:43 +0300

    add button

---
+++ Matrix.client/my-react-app/src/shared/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ text, id, onClick }) => (
+  <button id={id} className="contact-button" onClick={onClick}>
+    {text}
+  </button>
+);
+
+export default Button;
--- Matrix.client/my-react-app/src/shared/Consultation_link/Consultation.jsx
+++ Matrix.client/my-react-app/src/shared/Consultation_link/Consultation.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import './Consultation.css'
+import './Consultation.css';
+import Button from '../Button/Button';
 
 const Consultation = () => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const element = document.getElementById(event.target.getAttribute('id'));
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className='block_consultation'>
+      <div className="block_link_for_form">
         <div className="text_block">
-          <h3>Консультация по услугам</h3>
+          <h1>Консультация по услугам</h1>
           <p>Менеджеры компании с радостью ответят на ваши вопросы, произведут расчет стоимости услуг и подготовят индивидуальное коммерческое предложение.</p>
         </div>
         <div className="button_link">
-          <button>Задать вопрос</button>
+          <Button text='Задать вопрос' id='request' onClick={handleClick} />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Consultation
+export default Consultation;
